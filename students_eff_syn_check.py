3a5cb8edc7a619c35c6b7379c5ad66bd5627b60b
seed <seed@seed-labs.northamerica-northeast1-a.c.inlaid-woods-380820.internal>
2023-03-22 21:37:18 +0000

    yea

--- students_eff_syn_check.py
+++ students_eff_syn_check.py
@@ -10,6 +10,8 @@ import telnetlib
 ## Find Open TCP Ports ##
 #########################
 
+
+#---Helper that checks TCP responses---#
 def allow_tcp(pkt):
 	if pkt and pkt.haslayer(IP) and pkt.haslayer(TCP):
 		if pkt[TCP].flags & 0x3f == 0x12:   # SYN+ACK
@@ -21,39 +23,10 @@ def allow_tcp(pkt):
 		elif pkt[TCP].flags & 0x3f == 0x10: # FIN+ACK
 			return False
 
-srcport = random.randint(1024, 65535)
-ip = IP(dst="10.9.0.5")
-openPorts = []
-
-for port_number in range(2**12):
-	SYN = TCP(dport=port_number, sport=srcport, flags="S", seq=1000) #Make a SYN Packet
-	response = src1(ip/SYN)
-	if allow_tcp(response):
-		openPorts.append(port_number)
-
-
-with open("open_ports.txt", "w") as op:
-	op.write(openPorts)
 
 #########################################
 ## Check if these ports support Telnet ##
 #########################################
-
-telnet_ports = []
-
-for port_number in openPorts:
-	try:
-		telnet = telnetlib.Telnet("10.9.0.5")
-		answer = telnet.read_all()
-		if "User" in answer:
-			telnet_ports.append(port_number)
-	except:
-		continue
-
-with open("telnet_ports.txt", "w") as tp:
-	tp.write(telnet_ports)
-
-
 
 
 
